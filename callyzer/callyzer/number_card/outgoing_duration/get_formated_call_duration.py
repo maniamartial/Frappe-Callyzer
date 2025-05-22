5721658340d58402212976392345b614c53ebed5
faisalepty <jojomadready@gmail.com>
2025-05-22 03:44:23 +0300

    feat: add dashbord & number cards

---
+++ callyzer/callyzer/number_card/outgoing_duration/get_formated_call_duration.py
@@ -0,0 +1,19 @@
+import frappe
+def convert_sec(seconds):
+	hours = seconds // 3600
+	minutes = (seconds % 3600) // 60
+	seconds = seconds % 60
+
+	return f"{hours} hr {minutes} min {seconds} sec"
+
+@frappe.whitelist()
+def get_formated_duration():
+    call_logs = frappe.get_all("Call History Log", filter={"call_type": "Incoming"}, fields=["duration"])
+
+    total_duration = sum(log["duration"] for log in call_logs if log["duration"])
+    formated_duration = convert_sec(total_duration)
+
+    return {
+        "value": formated_duration,
+        "fieldtype": "Data",
+    }
